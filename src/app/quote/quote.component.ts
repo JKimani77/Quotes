c2ec319d43556df16aae7dc5312ec99ed26d49e8
Hewlett.Prd <jkkimani77@gmail.com>
2020-01-09 16:16:25 +0300

    created quotes array and logig for quote class

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+quotes= [
+  new Quote ("Life is too short to blend in","Paris Hilton","Michael Shoemaker"),
+  new Quote ("Is life not a thousand times too short for us to bore ourselves?","Friedrich Nietzche","Ellen Generous"),
+  new Quote ("Most of my stories are fantasy","Ray Bradbury","Lebrown James"),
+  new Quote ("A smile abroad is often a scowl at home","Alfred Lord Tennyson","Alesha Dickson"),
+  new Quote ("The man who has no imagination has no wings","Muhammad Ali","Mo Salad"),
+  new Quote ("I think that I shall never see a poem as lovely as a tree","Joyce Kilmer","Teafunny Haddish"),
+  new Quote ("We have got to put alot of money into changing behaviour","Bill Gates","Use code TFUE")
+];
 
   constructor() { }
 
