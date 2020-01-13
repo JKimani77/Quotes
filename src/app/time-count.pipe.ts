9a0e2360a2228537fc97d75118e3839751fc3c41
Hewlett.Prd <jkkimani77@gmail.com>
2020-01-13 13:04:19 +0300

    Added the pipe to the posttime property within the child component

--- src/app/time-count.pipe.ts
+++ src/app/time-count.pipe.ts
@@ -5,8 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimeCountPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: any): number {
+    let today:any = new Date(); //get current date and time
+    var dateDifference =Math.abs(value - today )// returns value in milliseconds
+    const secondsInAnHour= 3600; //60 seconds*60 minutes in an hour
+
+    var dateSecondsDifference=dateDifference*0.001; //converts to seconds
+
+    var dateCounter = Math.round(dateSecondsDifference/secondsInAnHour);
+
+    if (dateCounter >= 1) {
+      return dateCounter;
+    } else {
+      return 0;
+    }
   }
 
 }
